26104f75932d7a67399290e031a41b00d9a1c5fd
Alfredoeb9 <alfredoeb96@gmail.com>
2024-02-24 13:03:03 -0800

    [updat drizzle] updated drizzle orm and planetscale and updated db connection to follow nextjs commit https://github.com/t3-oss/create-t3-app/commit/0418663ec6cc370ed1e067f1ef68f0d5ca41af53#diff-3549675ccfec06463f2ab1bdb5c42b8ee4d323a1fcf6529915863bd387be1692

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,9 +4,11 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { env } from "@/env";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema, logger: true }
-);
+// export const db = drizzle(
+//   new Client({
+//     url: env.DATABASE_URL,
+//   }).connection(),
+//   { schema, logger: true }
+// );
+
+export const db = drizzle(new Client({ url: env.DATABASE_URL }), { schema, logger: true });
