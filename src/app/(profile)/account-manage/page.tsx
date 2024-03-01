a9fe8fbff86861d9feeedda7f4f73f9de8fcab93
Alfredoeb9 <alfredoeb96@gmail.com>
2024-02-29 17:31:48 -0800

    [account manage] updated ui for account manage to change ui depending on tab change

--- src/app/(profile)/account-manage/page.tsx
+++ src/app/(profile)/account-manage/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { api } from "@/trpc/react";
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
+import { useEffect, useState } from "react";
 // import { toast } from "react-toastify";
 
 // const CustomToastWithLink = () => (
@@ -12,11 +14,31 @@ import { useRouter } from "next/navigation";
 
 export default function AccountSettings() {
     const session = useSession();
-    const router = useRouter()
+    const router = useRouter();
+    const params = usePathname()
+    const search = useSearchParams()
+    const [currentKey, setCurrentKey] = useState<string>("account_settings")
 
-    if (session.status === 'unauthenticated') return router.push('/')
+    if (session.status === 'unauthenticated') return router.push('/');
+    
+
+    
 
     const getSingleUser = api.user.getSingleUser.useQuery({ email: session.data?.user.email as string}, { enabled: session.status === 'authenticated'});
+
+    useEffect(() => {
+        const doesHrefHaveId = window.location.href.split("#")
+        if (doesHrefHaveId.length === 2) {
+            if (doesHrefHaveId[1] == "connect-accounts") {
+                return setCurrentKey("gamer_tags")
+            } 
+            return
+        }
+
+        return () => {
+            setCurrentKey("")
+        }
+    }, [ router, window.location.href])
 
     if (getSingleUser.data === undefined) {
         router.push("/")
@@ -29,11 +51,35 @@ export default function AccountSettings() {
     }
 
     return (
-        <div className="container m-auto">
-            <h1 className="text-white text-xl md:text-2xl">Account Settings</h1>
+        <div className="container m-auto py-4">
+            <h1 className="text-white text-xl sm:text-2xl md:text-4xl">Account Settings</h1>
 
-            <div id="connect-accounts" className="text-white">
-                {getSingleUser.data.email}
+            <div className="text-white">
+                <div className="flex w-full flex-col">
+                    <Tabs aria-label="Options" color="primary" variant="underlined" classNames={{tab: "text-lg sm:text-xl"}} selectedKey={currentKey} onSelectionChange={(e) => setCurrentKey(e.toString())}>
+                        <Tab key="account_settings" id="account-settings" title={<div className="text-neutral-200 text-base sm:text-lg">ACCOUNT SETTINGS</div>}>
+                        <Card>
+                            <CardBody>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </CardBody>
+                        </Card>  
+                        </Tab>
+                        <Tab key="gamer_tags" id="connect-accounts" title={<div className="text-neutral-200 text-base sm:text-lg">GAMER TAGs/ IDs</div>}>
+                        <Card>
+                            <CardBody>
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </CardBody>
+                        </Card>  
+                        </Tab>
+                        <Tab key="social_media"title={<div className="text-neutral-200 text-base sm:text-lg">SOCIAL MEDIA</div>}>
+                        <Card>
+                            <CardBody>
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </CardBody>
+                        </Card>  
+                        </Tab>
+                    </Tabs>
+                </div> 
             </div>
         </div>
     )
