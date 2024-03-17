f5fb24274ca4d3d170ed41a7b6923f047bf8b4aa
Alfredoeb9 <alfredoeb96@gmail.com>
2024-03-16 17:31:53 -0700

    [ui] updated ui for pricing cards and cleaned up unused code

--- src/app/pricing/PricingCards.tsx
+++ src/app/pricing/PricingCards.tsx
@@ -7,57 +7,67 @@ import getStripe from "@/lib/utils/get-stripejs";
 import { pricingCards } from "@/lib/PricingCards";
 import Image from "next/image";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.min.css';
-
 
 export default function PricingCards() {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+        {pricingCards.map((card) => (
+          <Card
+            key={card.id}
+            isPressable
+            onPress={() => {
+              checkoutAction(card.credits)
+                .then(async (session) => {
+                  const stripe = await getStripe();
+                  if (stripe === null)
+                    return toast(
+                      "Stripe service down, please reach out to customer support",
+                      {
+                        position: "bottom-right",
+                        autoClose: false,
+                        closeOnClick: true,
+                        draggable: false,
+                        type: "error",
+                        toastId: 11,
+                      },
+                    );
+                  await stripe.redirectToCheckout({
+                    sessionId: session.id,
+                  });
+                })
+                .catch(() => {
+                  toast("You much be loggin in to buy credits", {
+                    position: "bottom-right",
+                    autoClose: 3500,
+                    closeOnClick: true,
+                    draggable: false,
+                    type: "error",
+                    toastId: 1,
+                  });
+                });
+            }}
+          >
+            <CardBody className="flex items-center justify-center">
+              <Image
+                src={"/images/mw3.png"}
+                alt={""}
+                width={200}
+                height={200}
+              />
+            </CardBody>
 
-    return (
-        <div>
-            <div className="gap-5 grid grid-cols-2 sm:grid-cols-4">
-                {pricingCards.map((card: any) => (
-                    <Card key={card.id} isPressable onPress={() => {
-                        checkoutAction(card.credits).then(async (session) => {
-                            const stripe = await getStripe();
-                            if (stripe === null) return toast('Stripe service down, please reach out to customer support', {
-                                position: "bottom-right",
-                                autoClose: false,
-                                closeOnClick: true,
-                                draggable: false,
-                                type: "error",
-                                toastId: 11                          
-                            });
-                            await stripe.redirectToCheckout({
-                                sessionId: session.id,
-                            });
-                        }).catch(() => {
-                            toast('You much be loggin in to buy credits', {
-                                position: "bottom-right",
-                                autoClose: false,
-                                closeOnClick: true,
-                                draggable: false,
-                                type: "error",
-                                toastId: 1                          
-                            })
-                        })
-                    }}>
-                        <CardBody className="flex justify-center items-center">
-                            <Image src={"/images/mw3.png"} alt={""} width={200} height={200} />
-                            
-                            
-                        </CardBody>
+            <CardFooter className="flex items-center justify-center">
+              <p className="mr-2">{card.credits} Credits</p>
 
-                        <CardFooter className="flex justify-center items-center">
-                                <p className="mr-2">{card.credits} Credits</p>
-
-                                <span className="bg-blue-400 px-3 py-4 rounded-md text-white hover:scale-105 shadow-xl">
-                                    Buy for ${card.cost}
-                                </span>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
-            <ToastContainer />
-        </div>
-    )
+              <span className="rounded-md bg-blue-400 px-3 py-4 text-white shadow-xl hover:scale-105">
+                Buy for ${card.cost}
+              </span>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+      <ToastContainer containerId={"pricing-card-toast-id"} />
+    </div>
+  );
 }
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -6,20 +6,31 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function Pricing() {
-    const { data: user, status } = useSession();
-    const router = useRouter()
+  const { data: user, status } = useSession();
+  const router = useRouter();
 
-    // status could === 
-    // unauthenticated || authenticated
+  // status could ===
+  // unauthenticated || authenticated
 
-    if (status === "unauthenticated") return router.push('/sign-in')
+  if (status === "unauthenticated") return router.push("/sign-in");
 
-    return (
-        <section className="flex justify-center items-center flex-col max-w-7xl w-full p-8 text-white">
-            <h1 className="text-3xl mb-2 text-bold">Buy Credits!</h1>
-            <p className="mb-2">Purchase credits to enter into tournaments or cash matches</p>
-            <p className="mb-2">Please review our <Link className="text-blue-500 hover:text-blue-600 transition-all" href={"/refund-policy"}>Refund Policy</Link> before buying credits. We do not issue refunds at this time.</p>
-            <PricingCards />
-        </section>
-    )
+  return (
+    <section className="m-auto flex w-full max-w-7xl flex-col items-center justify-center p-8 text-white">
+      <h1 className="text-bold mb-2 text-3xl">Buy Credits!</h1>
+      <p className="mb-2">
+        Purchase credits to enter into tournaments or cash matches
+      </p>
+      <p className="mb-2">
+        Please review our{" "}
+        <Link
+          className="text-blue-500 transition-all hover:text-blue-600"
+          href={"/refund-policy"}
+        >
+          Refund Policy
+        </Link>{" "}
+        before buying credits. We do not issue refunds at this time.
+      </p>
+      <PricingCards />
+    </section>
+  );
 }
