abaeb7d7c23d3b88d88daf2bc44bc20bb5661e02
Alfredoeb9 <alfredoeb96@gmail.com>
2024-03-31 18:53:15 -0700

    [Timer] updated timer to take in color and and updated if statement

--- src/app/_components/MatchTimer.tsx
+++ src/app/_components/MatchTimer.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 interface MatchTimerTypes {
   d2: Date;
   d1: Date;
+  color: string;
 }
 
-export default function MatchTimer({ d2, d1 }: MatchTimerTypes) {
+export default function MatchTimer({ d2, d1, color }: MatchTimerTypes) {
   const [days, setDays] = useState<string | number>(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -19,25 +20,26 @@ export default function MatchTimer({ d2, d1 }: MatchTimerTypes) {
   }
 
   useEffect(() => {
-    const totalSeconds = (t1 - t2) / 1000;
-    const i = setInterval(() => {
-      setDays(formatTime(Math.floor(totalSeconds / 3600 / 24)));
-      setHours(Math.floor(totalSeconds / 3600) % 24);
-      setMinutes(Math.floor(totalSeconds / 60) % 60);
-      setSeconds(Math.floor(totalSeconds % 60));
-    }, 1000);
-
-    return () => clearInterval(i);
-  }, [t1, t2]);
+    if (d2.valueOf() <= d1.valueOf()) {
+      const totalSeconds = (t1 - t2) / 1000;
+      const i = setInterval(() => {
+        setDays(formatTime(Math.floor(totalSeconds / 3600 / 24)));
+        setHours(Math.floor(totalSeconds / 3600) % 24);
+        setMinutes(Math.floor(totalSeconds / 60) % 60);
+        setSeconds(Math.floor(totalSeconds % 60));
+      }, 1000);
+      return () => clearInterval(i);
+    }
+  }, [d1, d2, t1, t2]);
   return (
     <>
       {d2.valueOf() <= d1.valueOf() ? (
-        <span className="text-xl sm:text-2xl">
+        <span className={`text-xl sm:text-2xl ${color}`}>
           {(days as number) > 0 && days + "D"} {hours > 0 && hours + "H"}{" "}
           {minutes > 0 && minutes + "M"} {seconds > 0 && seconds + "S"}
         </span>
       ) : (
-        <span className="text-xl sm:text-2xl">Match Started</span>
+        <span className={`text-xl sm:text-2xl ${color}`}>Match Started</span>
       )}
     </>
   );
