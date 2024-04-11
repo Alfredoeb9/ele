9b36b04263270aa469569934ba3f30a45f316675
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-11 15:42:32 -0700

    [trpc] updated trpc configuration for server and react

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -2,16 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
-import { createTRPCReact } from '@trpc/react-query';
+import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 
 import { type AppRouter } from "@/server/api/root";
 import { getUrl, transformer } from "./shared";
 
-export const api = createTRPCReact<AppRouter>();
-
-export function TRPCReactProvider(props: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({
+const createQueryClient = () =>
+  new QueryClient({
     defaultOptions: {
       queries: {
         // staleTime: 1000,
@@ -20,10 +18,25 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
         refetchOnWindowFocus: false,
         refetchOnReconnect: true,
         retry: false,
-        gcTime: 0
+        gcTime: 0,
       },
     },
-  }));
+  });
+
+let clientQueryClientSingleton: QueryClient | undefined = undefined;
+const getQueryClient = () => {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return createQueryClient();
+  }
+  // Browser: use singleton pattern to keep the same query client
+  return (clientQueryClientSingleton ??= createQueryClient());
+};
+
+export const api = createTRPCReact<AppRouter>();
+
+export function TRPCReactProvider(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
 
   const [trpcClient] = useState(() =>
     api.createClient({
@@ -35,10 +48,15 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
         }),
         unstable_httpBatchStreamLink({
           url: getUrl(),
-          transformer
+          transformer,
+          headers: () => {
+            const headers = new Headers();
+            headers.set("x-trpc-source", "nextjs-react");
+            return headers;
+          },
         }),
       ],
-    })
+    }),
   );
 
   return (
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,19 +1,11 @@
 import "server-only";
 
-import {
-  loggerLink,
-  TRPCClientError,
-} from "@trpc/client";
-import { createTRPCNext } from '@trpc/next';
-import { callTRPCProcedure  } from "@trpc/server";
-import { observable } from "@trpc/server/observable";
-import { type TRPCErrorResponse } from "@trpc/server/rpc";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-import { appRouter, type AppRouter } from "@/server/api/root";
+import { createCaller } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
-import { transformer } from "./shared";
+
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
@@ -27,39 +19,4 @@ const createContext = cache(() => {
   });
 });
 
-export const api = createTRPCNext<AppRouter>({
-  config(opts) {
-    return {
-      links: [
-        loggerLink({
-          enabled: (op) => process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
-        }),
-        /**
-         * Custom RSC link that lets us invoke procedures without using http requests. Since Server
-         * Components always run on the server, we can just call the procedure as a function.
-         */
-        () => ({ op }) => observable((observer) => {
-          createContext()
-            .then((ctx) => {
-              return callTRPCProcedure({
-                procedures: appRouter._def.procedures,
-                path: op.path,
-                getRawInput: op.input as any,
-                ctx,
-                type: op.type,
-              });
-            })
-            .then((data) => {
-              observer.next({ result: { data } });
-              observer.complete();
-            })
-            .catch((cause: TRPCErrorResponse) => {
-              observer.error(TRPCClientError.from(cause));
-            });
-        }),
-      ],
-    };
-  },
-  transformer
-});
+export const api = createCaller(createContext);
