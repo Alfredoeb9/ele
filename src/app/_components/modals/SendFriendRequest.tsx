aaf90fcb804be1eda3edd38f51898bad977a6e56
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-12 13:55:40 -0700

    [send friend request] clean up code

--- src/app/_components/modals/SendFriendRequest.tsx
+++ src/app/_components/modals/SendFriendRequest.tsx
@@ -1,127 +1,142 @@
-import {Modal, ModalContent, ModalHeader, ModalBody, useDisclosure, Input} from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  Input,
+} from "@nextui-org/react";
 import { useState } from "react";
 import { api } from "@/trpc/react";
 import { ToastContainer, toast } from "react-toastify";
 import { useSession } from "next-auth/react";
 
-import 'react-toastify/dist/ReactToastify.css';
-
+// import 'react-toastify/dist/ReactToastify.css';
 
 interface SendFriendProps {
-    open: boolean;
-    onOpenChange: () => void;
-    handleModalPath: (path: string) => void;
+  open: boolean;
+  onOpenChange: () => void;
+  handleModalPath: (path: string) => void;
 }
 
-export default function SendFriendRequest({ open, onOpenChange, handleModalPath }: SendFriendProps) {
-    const { onClose } = useDisclosure();
-    const [size, ] = useState<string>('md')
-    const [userName, setUserName] = useState<string>("");
+export default function SendFriendRequest({
+  open,
+  onOpenChange,
+  handleModalPath,
+}: SendFriendProps) {
+  const { onClose } = useDisclosure();
+  const [size] = useState<string>("md");
+  const [userName, setUserName] = useState<string>("");
 
-    const utils = api.useUtils()
+  const utils = api.useUtils();
 
-    const session = useSession();
+  const session = useSession();
 
-    const id = session.data?.user.id
-    const senderUser = session.data?.user.username
+  const id = session.data?.user.id;
+  const senderUser = session.data?.user.username;
 
-    const sendRequest = api.user.sendFriendRequest.useMutation({
-        onSuccess: async () => {
-            await utils.user.getNotifications.invalidate()
-            toast(`Friend request sent to ${userName}`, {
-                position: "bottom-right",
-                autoClose: 5000,
-                closeOnClick: true,
-                draggable: false,
-                type: "success",
-                toastId: 27          
-            })
-            setUserName("")
-        },
+  const sendRequest = api.user.sendFriendRequest.useMutation({
+    onSuccess: async () => {
+      await utils.user.getNotifications.invalidate();
+      toast(`Friend request sent to ${userName}`, {
+        position: "bottom-right",
+        autoClose: 5000,
+        closeOnClick: true,
+        draggable: false,
+        type: "success",
+        toastId: 27,
+      });
+      setUserName("");
+    },
 
-        onError: (e) => {
-            if (e.data?.zodError) {
-                e.data?.zodError?.fieldErrors.userName?.map((error) => {    
-                    if (error.includes("String must contain at least 1")) {
-                        toast('Please insert a username', {
-                            position: "bottom-right",
-                            autoClose: 5000,
-                            closeOnClick: true,
-                            draggable: false,
-                            type: "error",
-                            toastId: 39
-                        })
-                    }
-                })
-            } else if (e.message.includes("already sent user a friend request")) {
-                toast('You have already sent user a friend request', {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    closeOnClick: true,
-                    draggable: false,
-                    type: "error",
-                    toastId: 25          
-                })
-            } else if(e.message.includes("No user found")) {
-                toast(`User ${userName} does not exist`, {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    closeOnClick: true,
-                    draggable: false,
-                    type: "error",
-                    toastId: 25          
-                })
-            }
-        }
-    });
+    onError: (e) => {
+      if (e.data?.zodError) {
+        e.data?.zodError?.fieldErrors.userName?.map((error) => {
+          if (error.includes("String must contain at least 1")) {
+            toast("Please insert a username", {
+              position: "bottom-right",
+              autoClose: 5000,
+              closeOnClick: true,
+              draggable: false,
+              type: "error",
+              toastId: 39,
+            });
+          }
+        });
+      } else if (e.message.includes("already sent user a friend request")) {
+        toast("You have already sent user a friend request", {
+          position: "bottom-right",
+          autoClose: 5000,
+          closeOnClick: true,
+          draggable: false,
+          type: "error",
+          toastId: 25,
+        });
+      } else if (e.message.includes("No user found")) {
+        toast(`User ${userName} does not exist`, {
+          position: "bottom-right",
+          autoClose: 5000,
+          closeOnClick: true,
+          draggable: false,
+          type: "error",
+          toastId: 25,
+        });
+      }
+    },
+  });
 
-    return (
-        <>
-            <Modal 
-                size={size as 'md'} 
-                isOpen={open} 
-                onClose={() => {
-                    handleModalPath("")
-                    setUserName("")
-                    onClose()
-                }} 
-                onOpenChange={onOpenChange}
-            >
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1 text-red-600">Send Friend Request </ModalHeader>
-                            <ModalBody>
+  return (
+    <>
+      <Modal
+        size={size as "md"}
+        isOpen={open}
+        onClose={() => {
+          handleModalPath("");
+          setUserName("");
+          onClose();
+        }}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-red-600">
+                Send Friend Request{" "}
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  type="text"
+                  placeholder="username"
+                  onChange={(e) => setUserName(e.target.value)}
+                  value={userName}
+                />
 
-                            <Input 
-                                type="text" 
-                                placeholder="username" 
-                                onChange={(e) => setUserName(e.target.value)}
-                                value={userName}
-                            />
+                <div className="mt-2 flex justify-end gap-2 md:gap-3 xl:gap-4">
+                  <button className="text-red-500" onClick={onClose}>
+                    Cancel
+                  </button>
+                  <button
+                    className="rounded-2xl bg-green-500 p-3 text-white"
+                    disabled={sendRequest.isPending}
+                    onClick={(e) => {
+                      e.preventDefault();
 
-                            <div className="flex gap-2 md:gap-3 xl:gap-4 justify-end mt-2">
-                                <button className="text-red-500" onClick={onClose}>Cancel</button>
-                                <button className="bg-green-500 p-3 rounded-2xl text-white" disabled={sendRequest.isPending} onClick={(e) => {
-                                e.preventDefault();
-
-                                sendRequest.mutate({
-                                    userName,
-                                    id: id as string,
-                                    senderUserName: senderUser as string
-                                });
-                            }}>Send Request</button>
-                            </div>
-                                
-                            </ModalBody>
-                            <ToastContainer />
-                        </>
-                    )}
-                    
-                </ModalContent>
-                
-            </Modal>
-
-        </>
-    );
+                      sendRequest.mutate({
+                        userName,
+                        id: id!,
+                        senderUserName: senderUser!,
+                      });
+                    }}
+                  >
+                    Send Request
+                  </button>
+                </div>
+              </ModalBody>
+              <ToastContainer />
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
