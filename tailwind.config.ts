5101fa8d90029a8c22c9f394141addd1327247ca
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-12 15:17:57 -0700

    [images tailwind] added in images as a tailwind module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,15 +10,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        'elg-blue': '#002D72',
-        'elg-red': '#D50032',
-        'elg-white': '#fff'
+        "elg-blue": "#002D72",
+        "elg-red": "#D50032",
+        "elg-white": "#fff",
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       backgroundImage: {
         mw3_team_background: "url('/images/mw3_team_background.png')",
+        credit_bg: "url('/images/credits-bg.jpg)",
+        hero_bg: "url('/images/hero-bg.jpg')",
       },
     },
   },
