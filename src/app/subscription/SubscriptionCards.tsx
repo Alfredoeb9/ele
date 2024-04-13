b26e011aa838f22eea6cb14994a3b0c62f1507bc
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-13 14:04:36 -0700

    [format] updated code format

--- src/app/subscription/SubscriptionCards.tsx
+++ src/app/subscription/SubscriptionCards.tsx
@@ -7,84 +7,108 @@ import getStripe from "@/lib/utils/get-stripejs";
 import { subscriptionCards } from "@/lib/sharedData";
 import Image from "next/image";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.min.css';
 import Link from "next/link";
 
 interface SubscribtionTypes {
-    userId: string
+  userId: string;
 }
 
 export default function SubscriptionCards({ userId }: SubscribtionTypes) {
+  const CustomToastWithLink = () => (
+    <div>
+      User needs to{" "}
+      <Link href="/sign-in" className="text-blue-600 hover:text-blue-500">
+        sign in
+      </Link>{" "}
+      to subscribe.
+    </div>
+  );
 
-    const CustomToastWithLink = () => (
-        <div>
-            User needs to <Link href="/sign-in" className="text-blue-600 hover:text-blue-500">sign in</Link> to subscribe. 
-        </div>
-    );
+  return (
+    <div>
+      <div className="container m-auto">
+        {subscriptionCards.map(
+          (card: { id: number; cost: number; content: string[] }) => (
+            <Card key={card.id} isHoverable={false} isPressable={false}>
+              <CardBody className="m-2 flex items-center justify-center">
+                <Image
+                  src={"/images/mw3.png"}
+                  alt={""}
+                  width={200}
+                  height={200}
+                />
+                <div>
+                  <h3 className="font-semibold">
+                    Subscribing gives you the following:
+                  </h3>
+                  {card.content.map((content, i) => (
+                    <li key={i}>{content}</li>
+                  ))}
+                </div>
+              </CardBody>
 
-    return (
-        <div>
-            <div className="container m-auto">
-                {subscriptionCards.map((card: {id: number, cost: number, content: string[]}) => (
-                    <Card key={card.id} isHoverable={false} isPressable={false} >
-                        <CardBody className="flex justify-center items-center m-2">
-                            <Image src={"/images/mw3.png"} alt={""} width={200} height={200} />
-                            <div>
-                                <h3 className="font-semibold">Subscribing gives you the following:</h3>
-                                {card.content.map((content, i) => (
-                                    <li key={i}>{content}</li>
-                                ))}
-                            </div>
-                        </CardBody>
+              <CardFooter className="flex flex-col items-center justify-center">
+                <span
+                  className="rounded-md bg-blue-400 px-3 py-4 text-white shadow-xl transition-all hover:scale-105 hover:cursor-pointer"
+                  onClick={() => {
+                    checkoutSubscriptionAction(card.cost, userId)
+                      .then(async (session) => {
+                        const stripe = await getStripe();
+                        if (stripe === null)
+                          return toast(
+                            "Stripe service down, please reach out to customer support",
+                            {
+                              position: "bottom-right",
+                              autoClose: false,
+                              closeOnClick: true,
+                              draggable: false,
+                              type: "error",
+                              toastId: 32,
+                            },
+                          );
+                        await stripe.redirectToCheckout({
+                          sessionId: session.id,
+                        });
+                      })
+                      .catch((error) => {
+                        if (error.message.includes("You must be logged in")) {
+                          toast(CustomToastWithLink, {
+                            position: "bottom-right",
+                            autoClose: false,
+                            closeOnClick: true,
+                            draggable: false,
+                            type: "error",
+                            toastId: 33,
+                          });
+                        } else {
+                          toast(
+                            "Stripe error occured, please try again or create a support ticket",
+                            {
+                              position: "bottom-right",
+                              autoClose: false,
+                              closeOnClick: true,
+                              draggable: false,
+                              type: "error",
+                              toastId: 34,
+                            },
+                          );
+                        }
+                      });
+                  }}
+                >
+                  Buy for ${card.cost}
+                </span>
 
-                        <CardFooter className="flex justify-center items-center flex-col">
-                            <span className="bg-blue-400 px-3 py-4 rounded-md text-white hover:scale-105 hover:cursor-pointer shadow-xl transition-all" 
-                                onClick={() => {
-                                    checkoutSubscriptionAction(card.cost, userId).then(async (session) => {
-                                        const stripe = await getStripe();
-                                        if (stripe === null) return toast('Stripe service down, please reach out to customer support', {
-                                            position: "bottom-right",
-                                            autoClose: false,
-                                            closeOnClick: true,
-                                            draggable: false,
-                                            type: "error",
-                                            toastId: 32
-                                        });
-                                        await stripe.redirectToCheckout({
-                                            sessionId: session.id,
-                                        });
-                                    }).catch((error) => {
-                                        if (error.message.includes("You must be logged in")){
-                                            toast(CustomToastWithLink, {
-                                                position: "bottom-right",
-                                                autoClose: false,
-                                                closeOnClick: true,
-                                                draggable: false,
-                                                type: "error",
-                                                toastId: 33
-                                            })
-                                        } else {
-                                            toast('Stripe error occured, please try again or create a support ticket', {
-                                                position: "bottom-right",
-                                                autoClose: false,
-                                                closeOnClick: true,
-                                                draggable: false,
-                                                type: "error",
-                                                toastId: 34
-                                            })
-                                        }
-                                        
-                                    })
-                                }}>
-                                Buy for ${card.cost}
-                            </span>
-
-                            <p className="text-neutral-400 pt-4 sm:w-4/6">Membership will auto-renew at the end of each period. You may cancel at any time.</p>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
-            <ToastContainer containerId={"sub-toast"}/>
-        </div>
-    )
+                <p className="pt-4 text-neutral-400 sm:w-4/6">
+                  Membership will auto-renew at the end of each period. You may
+                  cancel at any time.
+                </p>
+              </CardFooter>
+            </Card>
+          ),
+        )}
+      </div>
+      <ToastContainer containerId={"sub-toast"} />
+    </div>
+  );
 }
--- src/app/subscription/actions.ts
+++ src/app/subscription/actions.ts
@@ -1,37 +1,38 @@
 "use server";
 
+import { env } from "@/env";
 import { stripe } from "@/lib/stripe";
 import { getServerSession } from "next-auth";
 
 export async function checkoutSubscriptionAction(plan: number, userId: string) {
-    const session = await getServerSession();
+  const session = await getServerSession();
 
-    if (!session?.user.email) {
-        throw new Error("You must be logged in to checkout!")
-    }
+  if (!session?.user.email) {
+    throw new Error("You must be logged in to checkout!");
+  }
 
-    const priceIds: Record<number, string> = {
-        5: process.env.SUB_PRICE_ID_SIMPLE!
-    }
-    const priceId = priceIds[plan];
+  const priceIds: Record<number, string> = {
+    5: process.env.SUB_PRICE_ID_SIMPLE!,
+  };
+  const priceId = priceIds[plan];
 
-    if (!priceId) {
-        throw new Error("invalid price id");
-    }
-    return stripe.checkout.sessions.create({
-        mode: "subscription",
-        payment_method_types: ["card", "us_bank_account"],
-        metadata: {
-            userId: userId,
-            email: session.user.email
-        },
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1
-            }
-        ],
-        success_url: `${process.env.REACT_APP_BASE_URL}/`,
-        cancel_url: `${process.env.REACT_APP_BASE_URL}/subscription`,
-    })
+  if (!priceId) {
+    throw new Error("invalid price id");
+  }
+  return stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card", "us_bank_account"],
+    metadata: {
+      userId: userId,
+      email: session.user.email,
+    },
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+    success_url: `${env.REACT_APP_BASE_URL}/`,
+    cancel_url: `${env.REACT_APP_BASE_URL}/subscription`,
+  });
 }
