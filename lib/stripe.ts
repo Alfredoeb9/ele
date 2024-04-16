3a996b0fd3548a7b1eb8461063d1d020344b9b72
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-16 12:46:46 -0700

    [stripe] updated stripe env variable

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
+import { env } from "@/env";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY);
