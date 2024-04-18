405272ee672ccaea1d36fce59efa4924d94af207
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-18 14:12:06 -0700

    [css] centered div

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -6,19 +6,28 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function Pricing() {
-    const { data: user, status } = useSession();
-    const router = useRouter()
+  const { data: user, status } = useSession();
+  const router = useRouter();
 
-    // status could === 
-    // unauthenticated || authenticated
+  // status could ===
+  // unauthenticated || authenticated
 
-    if (status === "unauthenticated") return router.push('/sign-in')
+  if (status === "unauthenticated") return router.push("/sign-in");
 
-    return (
-        <section className="flex justify-center items-center flex-col max-w-7xl w-full p-8 text-white">
-            <h1 className="text-4xl mb-2 text-bold">ELITE MEMBERSHIP!</h1>
-            <p className="mb-2">Please review our <Link className="text-blue-500 hover:text-blue-600 transition-all" href={"/refund-policy"}>Refund Policy</Link> before buying credits. We do not issue refunds at this time.</p>
-            <SubscriptionCards userId={user?.user.id as string} />
-        </section>
-    )
+  return (
+    <section className="m-auto flex w-full max-w-7xl flex-col items-center justify-center p-8 text-white">
+      <h1 className="text-bold mb-2 text-4xl">ELITE MEMBERSHIP!</h1>
+      <p className="mb-2">
+        Please review our{" "}
+        <Link
+          className="text-blue-500 transition-all hover:text-blue-600"
+          href={"/refund-policy"}
+        >
+          Refund Policy
+        </Link>{" "}
+        before buying credits. We do not issue refunds at this time.
+      </p>
+      <SubscriptionCards userId={user?.user.id as string} />
+    </section>
+  );
 }
