42492417276fa39c2cc7615cd1fa56b047abb6d8
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-18 15:19:40 -0700

    [clean up] cleaned up warnings and types

--- lib/utils/getIPAddress.ts
+++ lib/utils/getIPAddress.ts
@@ -1,6 +1,5 @@
 export const getIPAddress = async () => {
   const response = await fetch("https://api.ipify.org?format=json");
   const data = await response.json();
-
-  return data;
+  return data.ip as string;
 };
