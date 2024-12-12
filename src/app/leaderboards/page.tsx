499b9c11a69ced2bd96b5116b8868991a564c19f
Alfredo Barillas <alfredoeb96@gmail.com>
2024-12-12 12:16:18 -0800

    [leaderboard] implemented sort logic on query
    implemented sort query to desc on wins and updated frontend to account for isError and isLoading

--- src/app/leaderboards/page.tsx
+++ src/app/leaderboards/page.tsx
@@ -13,30 +13,49 @@ import {
 } from "@nextui-org/react";
 import { leaderBoardColumns } from "@/lib/sharedData";
 import { type SetStateAction, useMemo, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Leaderboards() {
   const [page, setPage] = useState(1);
   const rowsPerPage = 15;
 
   const columns = leaderBoardColumns;
-  const leaderboardData = api.user.getAllUsersRecords.useQuery();
+  const { data, isError, isLoading } = api.user.getAllUsersRecords.useQuery();
+
+  if (isError) {
+    toast(
+      "There was an error was this service, please refresh or submit a support ticket",
+      {
+        position: "bottom-right",
+        autoClose: 5000,
+        closeOnClick: true,
+        draggable: false,
+        type: "error",
+        toastId: 96,
+      },
+    );
+  }
 
   const items = useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
 
-    return leaderboardData.data?.slice(start, end);
-  }, [page, leaderboardData.data]);
+    return data?.slice(start, end);
+  }, [page, data]);
 
-  if (!leaderboardData.data) {
+  if (!data) {
     return <Spinner size="lg" />;
   }
 
-  const pages = Math.ceil(leaderboardData.data.length / rowsPerPage);
+  const pages = Math.ceil(data.length / rowsPerPage);
+
+  if (isLoading) {
+    <Spinner label="Loading..." color="warning" />;
+  }
 
   return (
     <div className="bg-white">
-      <div className="mx-auto w-full max-w-[960px]  py-10">
+      <div className="mx-auto w-full max-w-[960px] py-10">
         <h1 className="pb-5 text-6xl font-bold">Leaderboard</h1>
         <Table
           aria-label="ELE Leaderboard Page"
